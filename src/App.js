671b1c1e1b1c74a25b4f2a7fea07686954511b99
Chantal <chantal.maskell@bjss.com>
2023-07-18 16:36:10 +0100

    update flask

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 import Remaining from './components/Remaining';
@@ -6,43 +6,74 @@ import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AddExpenseForm from './components/AddExpenseForm';
 import { AppProvider } from './context/AppContext';
-import BarChart from './BarChart'
+import BarChart from './BarChart';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
 
 const App = () => {
-	<script src="https://d3js.org/d3.v7.min.js"></script>
+	const [profileData, setProfileData] = useState(null);
+
+	function getData() {
+		axios({
+			method: 'GET',
+			url: '/profile',
+		})
+			.then((response) => {
+				const res = response.data;
+				setProfileData({
+					profile_name: res.name,
+					about_me: res.about,
+				});
+			})
+			.catch((error) => {
+				if (error.response) {
+					console.log(error.response);
+					console.log(error.response.status);
+					console.log(error.response.headers);
+				}
+			});
+	}
+
 	return (
 		<AppProvider>
-			<div className='container'>
-				<h1 className='mt-3'>My Budget Planner</h1>
-				<div className='row mt-3'>
-					<div className='col-sm'>
+			<div className="container">
+				<h1 className="mt-3">My Budget Planner</h1>
+				<div className="row mt-3">
+					<div className="col-sm">
 						<Budget />
 					</div>
-					<div className='col-sm'>
+					<div className="col-sm">
 						<Remaining />
 					</div>
-					<div className='col-sm'>
+					<div className="col-sm">
 						<ExpenseTotal />
 					</div>
 				</div>
-				<h3 className='mt-3'>Expenses</h3>
-				<div className='row mt-3'>
-					<div className='col-sm'>
+				<h3 className="mt-3">Expenses</h3>
+				<div className="row mt-3">
+					<div className="col-sm">
 						<ExpenseList />
 					</div>
 				</div>
-				<h3 className='mt-3'>Add Expense</h3>
-				<div className='row mt-3'>
-					<div className='col-sm'>
+				<h3 className="mt-3">Add Expense</h3>
+				<div className="row mt-3">
+					<div className="col-sm">
 						<AddExpenseForm />
 					</div>
 				</div>
+				<BarChart data={[12, 5, 6, 6, 9, 10]} width={700} height={300} />
+				<header className="App-header">
+					<p>Click to get </p>
+					<button onClick={getData}>Click me</button>
+					{profileData && (
+						<div>
+							<p>Profile name: {profileData.profile_name}</p>
+							<p>About me: {profileData.about_me}</p>
+						</div>
+					)}
+				</header>
 			</div>
-			( <BarChart 
-				data={[12, 5, 6, 6, 9, 10]}
-				width={700}
-				height={300}
-			/> )
 		</AppProvider>
 	);
 };
